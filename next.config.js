75b6b102e06cdce3ce54050e712bfb8854e616d8
mohammad kaif <mohammadkaig051197@gmail.com>
2024-07-02 13:46:34 +0530

    some changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
 module.exports = {
   // basePath: '/lakshya-next',
   // assetPrefix: '/lakshya-next',
@@ -12,37 +10,6 @@ module.exports = {
       {
         source: "/courses",
         destination: "/course",
-      },
-      {
-        source: "/api/:path*",
-        destination: "https://api.lakshyacommerce.com/api/home/:path*", // Proxy to Backend
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
       },
     ];
   },
